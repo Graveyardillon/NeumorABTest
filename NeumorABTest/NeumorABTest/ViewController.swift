050b6fec955d3ab0cc563d7cb62b5d5a2cfe5349
Papillon6814 <so1rou.k@gmail.com>
2020-02-21 11:14:47 +0900

    Remote fetched

--- NeumorABTest/NeumorABTest/ViewController.swift
+++ NeumorABTest/NeumorABTest/ViewController.swift
@@ -7,14 +7,55 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    
+    var remoteConfig: RemoteConfig!
+    @IBOutlet weak var statusLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        remoteConfig = RemoteConfig.remoteConfig()
+        let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = 0
+        remoteConfig.configSettings = settings
+        
+        remoteConfig.setDefaults(fromPlist: "RemoteConfig")
+        
+        statusLabel.text = self.remoteConfig["loading_phrase"].stringValue
+        
+        remoteConfig.fetch(completionHandler: { (status, error) in
+            if status == .success {
+                print("Config fetched")
+                self.remoteConfig.activate(completionHandler: { (error) in
+                    
+                })
+            } else {
+                print("Config not fetched")
+            }
+        })
+        
+        self.displayWelcome()
     }
-
-
+    
+    private func displayWelcome() {
+        var welcomeMessage = self.remoteConfig["welcome_message"].stringValue
+        
+        if remoteConfig["welcome_message_caps"].boolValue {
+            welcomeMessage = welcomeMessage?.uppercased()
+        }
+        statusLabel.text = welcomeMessage
+    }
+    
+    @IBAction func testA(_ sender: Any) {
+        print("testA")
+    }
+    
+    @IBAction func testB(_ sender: Any) {
+        print("testB")
+    }
+    
 }
 
